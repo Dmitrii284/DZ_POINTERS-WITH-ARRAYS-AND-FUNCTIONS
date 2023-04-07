c6c7bc09758d07e889d6ae6043276a6b846acaa3
Дмитрий <@ADMIN>
2023-04-07 17:55:58 +0500

    22

--- DZ_POINTERS WITH ARRAYS AND FUNCTIONS/arr.cpp
+++ DZ_POINTERS WITH ARRAYS AND FUNCTIONS/arr.cpp
@@ -11,14 +11,28 @@ void mass(int *arr, const int length) {
 		std::cout << *(arr + i) << ", ";
 	std::cout << "\b\b]\n";
 }
-void p_mass(int *arr, const int length) {
-	
+//Task 1
+void p_mass(int *arr, const int length) {	
 	for (int i = 0; i < length - 1; i++)
 		if (i % 2 == 0) 		
-			std::swap(*arr, *(arr + 1));			
+			std::swap(*(arr + i), *(arr + i +1));			
 }
+//Task 2
+void func(int *a, int b) {
+	int result = 1;
+	for (int i = 1; i <= b; i++) 
+		result = result* *a;
+	
+		std::cout << "Exponentiation: " << result;
+	
+	std::cout << std::endl;
+}
+
+
 int main() {
 	setlocale(LC_ALL, "Rus");
+
+	//Task 1
 	const int size = 10;
 	int array[size]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	std::cout << "Initiali Array: \n";
@@ -26,5 +40,13 @@ int main() {
 	std::cout << "Sorted Array: \n";
 	p_mass(array,size);	
 	mass(array, size);
+	
+
+	//Task 2
+	int n = 10;
+	
+	func(&n, 3);
+
+
 	return 0;
 }
